db67e0c8d0457e202590a02233f5526dd0e963d9
Kerry Huang <104288325+kerry-huang-nyu@users.noreply.github.com>
2026-03-11 15:37:42 -0400

    added cd and ls

--- HW4/shell.c
+++ HW4/shell.c
@@ -1,12 +1,173 @@
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-void execute(){
+
+void file_open_dup(int target_fileno, int format){
+    char* file_name = strtok(NULL, " \n");
+
+    int fd;
+
+    if (file_name) {
+        fd = open(file_name, format, 0644);
+        if (fd < 0) {
+            perror("open input");
+            exit(EXIT_FAILURE);
+        }
+        dup2(fd, target_fileno); //Upon successful completion,
+    //  the new file descriptor is
+    //  returned.  Otherwise, a
+    //  value of -1 is returned and
+    //  the global integer variable
+    //  errno is set to indicate the
+    //  error.
+        close(fd);
+    }
+    else {
+        perror("file open failure"); 
+    }
+
+}
+
+
+int count_args(char *line) {
+
+    int argc = 0;
+    char *token = strtok(line, " \n");
+
+    while (token != NULL) {
+
+        if (strcmp(token, "<") == 0 ||
+            strcmp(token, ">") == 0 ||
+            strcmp(token, ">>") == 0 ||
+            strcmp(token, "2>") == 0 ||
+            strcmp(token, "&>") == 0) {
+
+            strtok(NULL, " \n");   // skip filename
+        }
+
+        else {
+            argc++;
+        }
+
+        token = strtok(NULL, " \n");
+    }
+
+    return argc;
+}
+
+void populate_argv(char *line, char **argv) {
+
+    int index = 0;
+    char *token = strtok(line, " \n");
+
+    while (token != NULL) {
+
+        if (strcmp(token, "<") == 0 ||
+            strcmp(token, ">") == 0 ||
+            strcmp(token, ">>") == 0 ||
+            strcmp(token, "2>") == 0 ||
+            strcmp(token, "&>") == 0) {
+
+            strtok(NULL, " \n");   // skip filename
+        }
+
+        else {
+            argv[index++] = token;
+        }
+
+        token = strtok(NULL, " \n");
+    }
+
+    argv[index] = NULL;
+}
+
+void parse_redirection(char *line) {
+    char* token;
+
+    token = strtok(line, " \n");
+
+    while (token != NULL) {
+
+        if (strcmp(token, "<") == 0) {
+            file_open_dup(STDIN_FILENO, O_RDONLY);
+        }
+
+        else if (strcmp(token, ">") == 0) {
+            file_open_dup(STDOUT_FILENO, O_WRONLY | O_CREAT | O_TRUNC);
+        }
+
+        else if (strcmp(token, ">>") == 0) {
+            file_open_dup(STDOUT_FILENO, O_WRONLY | O_CREAT | O_APPEND);
+        }
+
+        else if (strcmp(token, "2>") == 0) {
+            file_open_dup(STDERR_FILENO, O_WRONLY | O_CREAT | O_TRUNC);
+        }
+
+        else if (strcmp(token, "&>") == 0) {
+            file_open_dup(STDOUT_FILENO, O_WRONLY | O_CREAT | O_TRUNC);
+            dup2(STDOUT_FILENO, STDERR_FILENO);
+        }
+
+        else {
+            //nothing; 
+        }
+
+        token = strtok(NULL, " \n");
+    }
+}
+
+
+int execute(char *line, int argc, char* argv[]){
+
+    //parse the commands separated from parsing redirections 
+    pid_t pid = fork();
+
+    if (pid == -1){
+        perror("fork failed");
+        return -1;
+    }
+
+    else if (pid == 0){ // child process
+
+        parse_redirection(line);
+
+        if (argc == 0) {
+            exit(EXIT_SUCCESS);
+        }
+
+        execvp(argv[0], argv);
+
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    }
+
+    else {
+        int status;
+
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid failed");
+            return -1;
+        }
+
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        } 
+
+        else if (WIFSIGNALED(status)) {
+            return WTERMSIG(status) | 0x80;
+        }
+
+        return status;
+    }
 }
 
 int main(){ 
@@ -19,9 +180,11 @@ int main(){
         prompt_message = DEFAULT_MESSAGE; 
     }
 
-    
+
     char *line = NULL;
     size_t size = 0;
+
+    //int status; 
 
     while (true){
         //get a line of input 
@@ -33,33 +196,38 @@ int main(){
             break;
         }
 
-        //strtok to tokenize the line changinging the spaces into \0 
-        strtok(line, " "); 
-        strtok(line, "\n");
+        int argc = count_args(line);
+        char **argv = (char**) malloc(sizeof(char*) * (argc + 1));
+    
+        populate_argv(line, argv);
 
-        //exit 
-        char* command = line; 
-        //printf("line is %s\n", command);
+        if (argc == 0) {
+            continue;
+        }
+
+        char *command = argv[0];
+
         if (strcmp(command, "exit") == 0){
-            exit(EXIT_SUCCESS); 
+            exit(EXIT_SUCCESS);
         }
 
-        //isn't that gonna automatically execute? 
-        //no since the other process will execute but not the current 
-        if (strcmp(command, "cd") == 0){ 
-            chdir(command + 1); 
+        if (strcmp(command, "cd") == 0){
+            char *dir = argv[1];
+            if (dir == NULL) {
+                printf("a;sldkfja;slkdfj;aslkfj;aslkdfj;aslkdf");
+                dir = getenv("HOME");
+            }
+            if (chdir(dir) < 0){
+                fprintf(stderr, "chdir %s failed: %s\n", dir, strerror(errno));
+            }
         }
-        
-        //take the tokenized commands and execute them  
-        else { 
-            execute();
+        else {
+            int status = execute(line, argc, argv);
+            (void)status; // placeholder for $? support
         }
-
         //support the $? variable as well
 
     }
 
     return 0; 
 } 
-
-
--- HW4/test.md
+++ HW4/test.md
@@ -6,6 +6,10 @@ echo hello >> appendoutput.txt
 ls nonexistentfile 2> errors.txt
 < input.txt wc
 grep hello < test.md > output.txt
+ls > hello.txt > bello.txt : should expect to have bello with the ls contents only 
+wc < hello.txt < bello.txt : should expect to have wc applied only bello.txt only 
+cd > hello.txt : normally bash would create hello.txt but this time we will just expect to change directories with no side effects 
+
 
 $? 
     0: true
